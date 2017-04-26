004da3f5683f3ee5f562b49822a5de0ea51388c9
ralphcollett <ralph.collett@gmail.com>
2017-04-26 14:57:41 +0100

    [APB-704][RC] remove excessive test cases which are already covered.

--- src/test/scala/uk/gov/hmrc/agentmtdidentifiers/model/UtrSpec.scala
+++ src/test/scala/uk/gov/hmrc/agentmtdidentifiers/model/UtrSpec.scala
@@ -35,20 +35,12 @@ class UtrSpec extends FlatSpec with Matchers {
     Utr.isValid("") shouldBe false
   }
 
-  it should "be false when it is all zeros" in {
-    Utr.isValid("0000000000") shouldBe false
-  }
-
   it should "be false when it has fewer than 10 digits" in {
     Utr.isValid("200000") shouldBe false
   }
 
   it should "be false when it has non-digit characters" in {
     Utr.isValid("200000000B") shouldBe false
-  }
-
-  it should "be false when it us akk non-digit characters" in {
-    Utr.isValid("ABCDEFGHIJ") shouldBe false
   }
 
   it should "be false when it has non-alphanumeric characters" in {
