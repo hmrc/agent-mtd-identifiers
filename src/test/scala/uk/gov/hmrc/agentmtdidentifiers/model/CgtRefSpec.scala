40fd0800efd6430cafa943ab89d87da28787bb06
Paul Tan <20016950+JTY009@users.noreply.github.com>
2019-09-27 14:42:34 +0100

    [APB-4339][PT]: Update Library to add CgtRef

---
+++ src/test/scala/uk/gov/hmrc/agentmtdidentifiers/model/CgtRefSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentmtdidentifiers.model
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class CgtRefSpec extends FlatSpec with Matchers {
+
+  it should "be true for a valid CgtRef" in {
+    CgtRef.isValid("XMCGTP123456789") shouldBe true
+  }
+
+  it should "be false when CgtRef's character 2 is lowercase" in {
+    CgtRef.isValid("XmCGTP123456789") shouldBe false
+  }
+
+  it should "be false when CgtRef's character 1 is not X" in {
+    CgtRef.isValid("ZMCGTP123456789") shouldBe false
+  }
+
+  it should "be false when CgtRef CGTP is lowercase" in {
+    CgtRef.isValid("XMcgtp123456789") shouldBe false
+  }
+
+  it should "be false when X[A-Z]CGTP is missing" in {
+    CgtRef.isValid("123456789") shouldBe false
+  }
+
+  it should "be false when CgtRef is empty" in {
+    CgtRef.isValid("") shouldBe false
+  }
+
+  it should "be false when CgtRef is too short" in {
+    CgtRef.isValid("XMCGTP1234") shouldBe false
+  }
+
+  it should "be false when CgtRef is too long" in {
+    CgtRef.isValid("XMCGTP1234567890") shouldBe false
+  }
+
+}
