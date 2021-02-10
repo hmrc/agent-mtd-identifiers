25f97338f8675020ca2c8c83193a53f3d42e0a38
geoffwatson <9043337+geoffreywatson@users.noreply.github.com>
2021-02-10 12:14:07 +0000

    [APB-5081][GW] cross compile for play 28

--- project/PlayCrossCompilation.scala
+++ project/PlayCrossCompilation.scala
@@ -1,10 +1,4 @@
 import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
 import uk.gov.hmrc.playcrosscompilation.PlayVersion
 
-object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = PlayVersion.Play26) {
-  override def playCrossScalaBuilds(scalaVersions: Seq[String]): Seq[String] =
-    playVersion match {
-      case PlayVersion.Play26 => scalaVersions
-      case PlayVersion.Play27 => scalaVersions.filter(version => version.startsWith("2.12"))
-    }
-}
+object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = PlayVersion.Play27)
