0592ec8d475c96abca1b380e31a6289b6c58eb3b
geoffwatson <9043337+geoffreywatson@users.noreply.github.com>
2021-09-29 16:34:01 +0100

    [APB-5367][GW] add PPT identifier

---
+++ src/test/scala/uk/gov/hmrc/agentmtdidentifiers/model/PPTSpec.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentmtdidentifiers.model
+
+import org.scalacheck.Gen
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class PPTSpec extends FlatSpec with Matchers with ScalaCheckPropertyChecks {
+
+  val permittedChars = Gen.oneOf("ABCDEFGHIJKLMNOQPRSTUVWXYZ0123456789")
+  val validPptId = Gen.listOfN(15, permittedChars).map(_.toArray).map(new String(_))
+
+  it should "be true for a valid PPT" in {
+    forAll(validPptId) { pptId =>
+      PPT.isValid(pptId) shouldBe true
+    }
+  }
+
+  it should "be false when it has more than 15 digits" in {
+    PPT.isValid("0000000000000000") shouldBe false
+  }
+
+  it should "be false when it is empty" in {
+    PPT.isValid("") shouldBe false
+  }
+
+  it should "be false when it contains lowercase alpha-numeric" in {
+    PPT.isValid("abcde1234567890") shouldBe false
+  }
+
+  it should "be false when it has non-alphanumeric characters" in {
+    PPT.isValid("00000000000000!") shouldBe false
+  }
+
+}
