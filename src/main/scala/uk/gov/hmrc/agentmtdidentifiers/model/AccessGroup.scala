24216399cb052e8dc6d996a19a8678b357295cf2
Unmesh Malvankar <unmesh.malvankar1@digital.hmrc.gov.uk>
2022-06-22 11:20:30 +0100

    [APB-6054][UM] Adds model objects for group summaries and client list (#52)

--- src/main/scala/uk/gov/hmrc/agentmtdidentifiers/model/AccessGroup.scala
+++ src/main/scala/uk/gov/hmrc/agentmtdidentifiers/model/AccessGroup.scala
@@ -17,7 +17,9 @@
 package uk.gov.hmrc.agentmtdidentifiers.model
 
 import play.api.libs.json.{Json, OFormat}
+import uk.gov.hmrc.agentmtdidentifiers.model.GroupId.{ENCODING, SPLITTER}
 
+import java.net.{URLDecoder, URLEncoder}
 import java.time.LocalDateTime
 
 case class AccessGroup(
@@ -33,4 +35,60 @@ case class AccessGroup(
 
 object AccessGroup {
   implicit val formatAccessGroup: OFormat[AccessGroup] = Json.format[AccessGroup]
+}
+
+case class AccessGroupSummary(groupId: String, groupName: String, clientCount: Int, teamMemberCount: Int)
+
+object AccessGroupSummary {
+
+  def convert(accessGroup: AccessGroup): AccessGroupSummary =
+    AccessGroupSummary(
+      GroupId(accessGroup.arn, accessGroup.groupName).encode,
+      accessGroup.groupName,
+      accessGroup.clients.fold(0)(_.size),
+      accessGroup.teamMembers.fold(0)(_.size)
+    )
+
+  implicit val formatAccessGroupSummary: OFormat[AccessGroupSummary] = Json.format[AccessGroupSummary]
+}
+
+case class AccessGroupSummaries(groups: Seq[AccessGroupSummary], unassignedClients: Set[Client])
+
+object AccessGroupSummaries {
+  implicit val format: OFormat[AccessGroupSummaries] = Json.format[AccessGroupSummaries]
+}
+
+case class ClientList(assigned: Set[Client], unassigned: Set[Client])
+
+object ClientList {
+  implicit val format: OFormat[ClientList] = Json.format[ClientList]
+}
+
+case class GroupId(arn: Arn, groupName: String) {
+
+  def encode: String =
+    URLEncoder.encode(arn.value + SPLITTER + groupName, ENCODING)
+}
+
+object GroupId {
+
+  private val ENCODING = "UTF-8"
+  private val SPLITTER = "~"
+
+  def decode(gid: String): Option[GroupId] =
+    Option(gid).flatMap { gid =>
+      if (gid.contains(" ")) {
+        None
+      } else {
+        URLDecoder.decode(gid, ENCODING).split(SPLITTER) match {
+          case parts if parts.length != 2 =>
+            None
+          case parts if Arn.isValid(parts(0)) =>
+            Option(GroupId(Arn(parts(0)), parts(1).trim))
+          case _ =>
+            None
+        }
+      }
+    }
+
 }
