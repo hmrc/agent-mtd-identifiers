a787a8d48be00336b67bd05284f000c60b957f7c
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-01-09 16:44:34 +0000

    [CS] new year copyright

--- src/main/scala/uk/gov/hmrc/agentmtdidentifiers/model/UserDetails.scala
+++ src/main/scala/uk/gov/hmrc/agentmtdidentifiers/model/UserDetails.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,4 @@ object UserDetails {
     UserDetails(userId = Some(agentUser.id), name = Some(agentUser.name))
 
   implicit val formats: Format[UserDetails] = Json.format
-
-  def fromAgentUser(agentUser: AgentUser): UserDetails = UserDetails(userId = Some(agentUser.id), name = Some(agentUser.name))
 }
