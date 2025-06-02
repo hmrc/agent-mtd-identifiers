0f9b40416ef141528e26553a19c1938ca39af05b
lewishay <29063729+lewishay@users.noreply.github.com>
2025-06-02 11:16:59 +0100

    Address platops bot comments

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -8,7 +8,6 @@ object LibDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest" %% "scalatest" % "3.2.19" % Test,
-    "org.pegdown" % "pegdown" % "1.6.0" % Test,
     "org.scalacheck" %% "scalacheck" % "1.17.0" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.64.0" % Test
   )
